6e22289f6bfb6cccaa61008314c19ad038c1f4f0
Thai on Cloud9 <dtinth@spacet.me>
2019-05-01 10:46:10 +0000

    Lambda is working now!

---
+++ README.md
@@ -0,0 +1,15 @@
+## Development
+
+### Running the bot locally
+
+1. Install the Netlify CLI
+
+    ```
+    npm install netlify-cli -g
+    ```
+
+2. Run Netlify 
+
+    ```
+    netlify dev --live
+    ```
--- functions/bot/bot.js
+++ functions/bot/bot.js
@@ -1,18 +1,46 @@
 const {VM} = require('vm2')
+const line = require('@line/bot-sdk');
 
-async function hello() {
+const config = {
+  channelAccessToken: process.env.LINE_CHANNEL_ACCESS_TOKEN,
+  channelSecret: process.env.LINE_CHANNEL_SECRET
+}
+
+const client = new line.Client(config)
+
+async function execute(code) {
   const vm = new VM({
     timeout: 100,
     sandbox: {}
   })
-  let result = vm.run('1+1')
+  let result = vm.run(code)
   return result
+}
+
+async function handleEvent(event) {
+  if (event.type !== 'message') {
+    return `Sorry, I don’t handle events of type ${event.type}`
+  }
+  if (event.message.type !== 'text') {
+    return `Sorry, I don’t handle messages of type ${event.message.type}`
+  }
+  const result = await execute(event.message.text)
+  const results = [
+    require('util').inspect(result, { depth: 5 })
+  ]
+  await client.replyMessage(event.replyToken, {
+    type: 'text',
+    text: String(results)
+  })
 }
 
 exports.handler = async function(event, context) {
   try {
-    const body = await hello()
-    return { statusCode: 200, body }
+    const eventBody = JSON.parse(event.body)
+    const results = await Promise.all(
+      eventBody.events.map(handleEvent)
+    )
+    return { statusCode: 200, body: results.join('\n') }
   } catch (err) {
     console.error(err)
     return { statusCode: 500, body: err.toString() }
