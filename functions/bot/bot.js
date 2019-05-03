bf330c89acc6849901390165ab0b556e55473e43
Thai on Cloud9 <dtinth@spacet.me>
2019-05-03 12:30:41 +0000

    Restructure for Lambda

--- functions/bot/bot.js
+++ functions/bot/bot.js
@@ -1,6 +1,6 @@
 const {VM} = require('vm2')
 const line = require('@line/bot-sdk');
-const {execute} = require('../lib/ScriptExecutor')
+const {execute} = require('./lib/ScriptExecutor')
 const config = {
   channelAccessToken: process.env.LINE_CHANNEL_ACCESS_TOKEN,
   channelSecret: process.env.LINE_CHANNEL_SECRET
