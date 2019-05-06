3de3fe546c51e230fe6ba29dabfdc2572803f0a4
Thai on Cloud9 <dtinth@spacet.me>
2019-05-06 06:17:47 +0000

    Forgot to wrap page into functions

--- src/pages/sharing.js
+++ src/pages/sharing.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export default <a
+export default () => <a
   href="https://www.notion.so/Under-construction-615adbcf50ee407191355222f4e6336b"
   >Under construction</a
 >
